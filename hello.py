e5b6e9667667cae9fd30aa423bfc627092bfee41
GageGaskins <tgg8ec@virginia.edu>
2015-02-16 14:22:44 -0500

    Activity 1 Commit

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 __author__ = 'tggec'
 
+from helper import greeting
 
-print("Hello World!");
+if __name__ == '__main__':
+    greeting("Hello")
---
+++ helper.py
@@ -0,0 +1,5 @@
+__author__ = 'tgg8ec'
+
+def greeting(msg):
+	print(msg)
+
